620df8d10ef5a99650e6fe3272e21ce0c0af325d
Tran Trong Hien <tronghientran18@gmail.com>
2024-07-25 17:31:21 +0700

    config axios

--- src/utils/axios-client.ts
+++ src/utils/axios-client.ts
@@ -1,34 +1,28 @@
-// import queryString from 'query-string'
+import axios from "axios";
+import queryString from "query-string";
 
-// // Get access token
-// const ISSERVER = typeof window === 'undefined'
-// let accessToken = ""
+// Get access token
+// accessToken = sessionStorage.getItem("erpAccessToken");
+const accessToken = localStorage.getItem("token");
 
-// if (!ISSERVER) {
-//   if (!accessToken) {
-//     accessToken = sessionStorage.getItem('erpAccessToken')
-//   }
-// }
-// /* Creating a new axios client with the baseURL, headers, and paramsSerializer. */
-// export const axiosClient = axios.create({
-//   baseURL: process.env.API_URL,
-//   headers: {
-//     'Content-Type': 'application/json',
-//     Authorization: 'Bearer ' + accessToken,
-//     'x-api-key': process.env.APP_FAST_API_KEY ?? ""
-//   },
-//   paramsSerializer: (params) => queryString.stringify(params),
-// })
-// /* Intercepting the response and returning the data. */
-// axiosClient.interceptors.response.use(
-//   (response) => {
-//     if (response && response.data) {
-//       return response.data
-//     }
-//     return response
-//   },
-//   (error) => {
-//     return Promise.reject(error)
-//   }
-// )
-// query-string
+/* Creating a new axios client with the baseURL, headers, and paramsSerializer. */
+export const axiosClient = axios.create({
+  baseURL: process.env.API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + accessToken,
+  },
+  paramsSerializer: (params) => queryString.stringify(params),
+});
+/* Intercepting the response and returning the data. */
+axiosClient.interceptors.response.use(
+  (response) => {
+    if (response && response.data) {
+      return response.data;
+    }
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
